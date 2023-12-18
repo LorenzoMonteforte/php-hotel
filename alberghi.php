9e9ef2eee0e42c2800abff2741f327f2dbdd3545
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-18 16:00:02 +0100

    Milestone 1

--- alberghi.php
+++ alberghi.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="alberghi.css">
     <?php
         $hotels = [
@@ -69,5 +70,36 @@
             }
         ?>
     </ul>
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                for($i=0; $i<sizeof($hotels); $i++){
+                    $parking;
+                    if($hotels[$i]["parking"]==true){
+                        $parking = "Si";
+                    }else{
+                        $parking = "No";
+                    }
+                    echo "<tr>
+                            <td>".$hotels[$i]["name"]."</td>
+                            <td>".$hotels[$i]["description"]."</td>
+                            <td>".$parking."</td>
+                            <td>".$hotels[$i]["vote"]."</td>
+                            <td>".$hotels[$i]["distance_to_center"]."</td>
+                          </tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
